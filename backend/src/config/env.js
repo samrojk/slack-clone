65eaa3db93857b759c0bc907bcd93cb72fb065e5
Samroj Khan <samrojk7744@gmail.com>
2026-05-05 14:57:55 +0530

    vercel setup

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 export const ENV = {
     PORT: process.env.PORT || 5001,
     MONGO_URI: process.env.MONGO_URI,
-    MONGO_ENV: process.env.MONGO_ENV,
+    NODE_ENV: process.env.NODE_ENV,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     STREAM_API_KEY: process.env.STREAM_API_KEY,
