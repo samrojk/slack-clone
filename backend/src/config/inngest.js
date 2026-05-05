671cf9fb3eeed4a53c14df186e0559059691f35b
Samroj Khan <samrojk7744@gmail.com>
2026-05-05 15:14:03 +0530

    dp issue resolved

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -37,5 +37,3 @@ const deleteUserFromDB = inngest.createFunction(
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUser, deleteUserFromDB];
-
-// inngest is updated final...
